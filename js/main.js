1a294f1b359c1e36818038837824a6aa27f6e893
Lucas Stiening <lucasstiening@gmail.com>
2024-06-21 17:24:33 -0400

    daily code-wars

--- js/main.js
+++ js/main.js
@@ -171,3 +171,18 @@ function getCount(str) {
   }
   return totVowels
 }
+/* kata 7kyu: In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater than or equal to p = 1200 inhabitants?
+*/
+
+
+// my proposed solution :
+function nbYear(p0, percent, aug, p) {
+  let years = 0
+	for (let i = 1; p0 < p; i++) {
+    let inc = ((p0 * (percent / 100)) + aug);
+    p0 += Math.floor(inc)
+    years = i 
+  }
+  
+  return years ;
+}
