832e243c4147c2ef0cee32dde90f0d69620f0ceb
Lucas Stiening <lucasstiening@gmail.com>
2024-06-24 14:40:18 -0400

    daily code wars

--- js/main.js
+++ js/main.js
@@ -197,3 +197,29 @@ What ways of achieving this do you know?
 function numberToString(num) {
  return String(num)
 }
+
+/* kata 7kyu: You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters. 
+
+
+
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
+*/
+
+
+// my proposed solution :
+function getMiddle(s){
+  let sLen = s.length;
+  if (sLen % 2 != 0) 
+  {
+    let start = (sLen - 1) / 2;
+    return s.slice(start, start + 1)
+  } 
+  else 
+  {
+    let start = sLen / 2 - 1;
+    return s.slice(start, start + 2);
+  }
+}
